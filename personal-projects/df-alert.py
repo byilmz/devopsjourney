b0f62cc3f1cc95dd79dc984c27d5ccf37a864681
Burak Yilmaz <yilm.b3@gmail.com>
2022-09-30 01:07:44 +0100

    fixed rounding issue

--- personal-projects/df-alert.py
+++ personal-projects/df-alert.py
@@ -9,9 +9,10 @@ total, used, free = shutil.disk_usage(path)
 # Disk usage statistics
 # about the given path
   
-total_2_dp = round(total, 2) # this doesn't work
-print(type(total_2_dp))
-print(f"Disk Usage:\nTotal: {total_2_dp / 2**30} GiB, Used:{used / 2**30} GiB, Free:{free / 2**30} GiB")
+total_2_dp = round(total / 2**30, 2)
+used_2_dp = round(used / 2**30, 2)
+free_2_dp = round(free / 2**30, 2)
+ # round to 2 decimal points
+
+print(f"Disk Usage:\nTotal: {total_2_dp} GiB, Used: {used_2_dp} GiB, Free: {free_2_dp} GiB")
 # show disk usage in cli
-
-
