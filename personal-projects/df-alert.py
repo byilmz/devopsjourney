48a423fa4d4e5ef9e1482010eb7c9d33d3225f85
Burak Yilmaz <yilm.b3@gmail.com>
2022-09-30 12:00:06 +0100

    adding conditional statements

--- personal-projects/df-alert.py
+++ personal-projects/df-alert.py
@@ -12,7 +12,17 @@ total, used, free = shutil.disk_usage(path)
 total_2_dp = round(total / 2**30, 2)
 used_2_dp = round(used / 2**30, 2)
 free_2_dp = round(free / 2**30, 2)
- # round to 2 decimal points
+# round to 2 decimal points
 
-print(f"Disk Usage:\nTotal: {total_2_dp} GiB, Used: {used_2_dp} GiB, Free: {free_2_dp} GiB")
-# show disk usage in cli
+threshold = 80
+
+
+if (used / total)*100 >= threshold:
+    print(total)
+    # if disk usage is greater than or equal to 80%
+    # run diskcleanup (placeholder set currently)
+
+else: 
+    print(f"Disk Usage:\nTotal: {total_2_dp} GiB, Used: {used_2_dp} GiB, Free: {free_2_dp} GiB")
+    # otherwise, show disk usage in cli
+   
