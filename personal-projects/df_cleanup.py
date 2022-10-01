b8907020df3f35e869bbd8b3cdc3c7f39555f336
Burak Yilmaz <yilm.b3@gmail.com>
2022-10-01 17:23:07 +0100

    disk clean up function created

--- personal-projects/df_cleanup.py
+++ personal-projects/df_cleanup.py
@@ -1,8 +1,30 @@
+#!/usr/bin/env python3
+
 import os, time, shutil
 # importing required modules
   
-path = "${pwd}"
+path = os.getcwd()
 # setting path to current working directory
+
+def dfcleanup():
+
+    timestamp = os.path.getmtime(path)
+
+    days = 40
+
+    s_in_days = time.time() - (days * 24 * 60 * 60) 
+    # time.time() is current time relative to epoch => Jan 1st 1970 00:00:00 UTC
+    # minus 40 days in seconds
+
+    for file in os.listdir(path):
+        
+        filename = os.fsdecode(file)
+        
+        if timestamp >= s_in_days:
+            os.remove(filename)
+
+            break
+
   
 total, used, free = shutil.disk_usage(path)
 # Disk usage statistics
@@ -17,30 +39,12 @@ threshold = 80
 # define threshold at which disk cleanup should be performed 
 
 if (used / total)*100 >= threshold:
-    print(total)
+    dfcleanup()
+    print("Clean up complete (:")
+    #alert smtp && rundiskcleanup
+    
     # if disk usage is greater than or equal to threshold
-    # run diskcleanup (placeholder set currently)
 
 else: 
     print(f"Disk Usage:\nTotal: {total_2_dp} GiB, Used: {used_2_dp} GiB, Free: {free_2_dp} GiB")
     # otherwise, show disk usage in cli
-
-log_path = r"/var/log"
- 
-# file modification
-timestamp = os.path.getmtime(log_path)
-
-days = 40
-
-s_in_days = time.time() - (days * 24 * 60 * 60) 
-# time.time() is current time relative to epoch => Jan 1st 1970 00:00:00 UTC
-# minus 40 days in seconds
-
-for file in os.listdir(log_path):
-    
-    filename = os.fsdecode(file)
-    
-    if timestamp >= s_in_days:
-        os.remove(filename)
-
-        break
